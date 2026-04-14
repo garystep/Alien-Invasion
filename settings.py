6f4b5dea9aba60b6024b8025123891e19286168e
garystep <sg6787944@gmail.com>
2026-04-14 15:14:16 -0400

    init bullet settings

--- settings.py
+++ settings.py
@@ -2,7 +2,16 @@ class Settings:
     """Class to hold settings for the application."""
     
     def __init__(self):
+        # Initialize the game's settings.
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        
+        # Ship settings
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0 
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
