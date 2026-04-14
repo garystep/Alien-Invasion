29e33e150d374ccc574965ad1fd7932dc03aa8f2
garystep <sg6787944@gmail.com>
2026-04-14 14:51:03 -0400

    seperated key up and key down events into helper methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,17 +36,25 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.moving_left = True
+            elif event.type == pygame.KEYDOWN:   
+                self._check_keydown_events(event)
 
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.moving_left = False
+                self._check_keyup_events(event)
+        
+    def _check_keydown_events(self, event):    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                self.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
