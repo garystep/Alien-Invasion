901b5ac3cc92bb8db1bbab216d02b97e1a4df645
garystep <sg6787944@gmail.com>
2026-04-17 14:32:48 -0400

    simple alien movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -106,6 +106,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
@@ -124,6 +128,7 @@ class AlienInvasion:
             self.update()
             self.bullets.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
