37920fda0cbc2a8f4846f285b85b72c1a3801e13
garystep <sg6787944@gmail.com>
2026-04-17 14:07:59 -0400

    nested while loop in create fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,16 +29,27 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
+        # Create an alien and find the number of aliens in a row.
+        # Spacing between each alien is equal to one alien width.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - (2 * alien_width)):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = new_alien.x
-            self.aliens.add(new_alien)
-            current_x += alien_width * 2
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - (3 * alien_height) - self.ship.rect.height):
+            while current_x < (self.settings.screen_width - (2 * alien_width)):
+                self._create_alien(current_x, current_y)
+                current_x += alien_width * 2
+                
+            current_x = alien_width
+            current_y += alien_height * 2
+
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it in the row."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
     def update(self):
         """Update the ship's position based on the movement flag."""
