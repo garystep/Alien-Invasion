b96ba4831d905d4be495cf8aa1912ece50530db3
garystep <sg6787944@gmail.com>
2026-04-18 14:28:23 -0400

    centering the ship after the pause

--- ship.py
+++ ship.py
@@ -19,6 +19,11 @@ class Ship:
         # Store a float for the ship's horizontal position.
         self.x = float(self.rect.x)
 
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
