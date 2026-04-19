685432e945bbd5cdc0a22c0775db54ba55bc6348
garystep <sg6787944@gmail.com>
2026-04-19 15:14:53 -0400

    init dynamic settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -106,6 +106,9 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+
+            self.settings.initialize_dynamic_settings()
+
             # Reset the game settings.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -168,6 +171,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
