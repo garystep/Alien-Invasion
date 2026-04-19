0840c7ffd2595024701e50cc05229bcee59d0b85
garystep <sg6787944@gmail.com>
2026-04-19 15:32:06 -0400

    draw the score to the screen

--- scoreboard.py
+++ scoreboard.py
@@ -17,3 +17,19 @@ class Scoreboard:
 
         # Prepare the initial score image.
         self.prep_score()
+
+    def prep_score(self):
+        """Turn the score into a rendered image."""
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
+        self.score_image = self.font.render(score_str, True, self.text_color,
+                                            self.settings.bg_color)
+
+        # Display the score at the top right of the screen.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """Draw the score to the screen."""
+        self.screen.blit(self.score_image, self.score_rect)
